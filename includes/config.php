f889392064d285cddee23617f5a1a873a30402f9
dferlema <d.fernandolema@gmail.com>
2025-07-07 21:14:40 -0500

    configure file config.php

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $dbname = '01_calf';
 $username = 'root';
-$password = '';
+$password = 'Ube2025.';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
